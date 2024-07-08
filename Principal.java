1c3f377a02a66ec1486a1c7cd6ecdd904c5b4636
CarlosManuel2505 <163787386+CarlosManuel2505@users.noreply.github.com>
2024-07-07 20:45:39 -0600

    Add files via upload

---
+++ Principal.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Pregunta4;
+
+public class Principal {
+public static void main(String [] args){
+Nombre nombre=new Nombre();
+nombre.setVisible(true);
+nombre.setResizable(false);
+nombre.setLocationRelativeTo(null);
+}
+    
+}
