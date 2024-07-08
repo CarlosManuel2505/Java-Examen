a187f48af0332b591f3fd49d87d001916d236a1b
CarlosManuel2505 <163787386+CarlosManuel2505@users.noreply.github.com>
2024-07-07 20:56:53 -0600

    Add files via upload
    Pregunta2

---
+++ String/Pregunta2.java
@@ -0,0 +1,11 @@
+
+package String;
+import java.util.Scanner;
+public class Nombre {
+    public static void main (String [] args){
+        Scanner scan=new Scanner(System.in);
+        System.out.println("Carlos Manuel");
+        String nombre=scan.nextLine();
+        System.out.println("Bienvenido" + nombre);
+    }
+}
